41f84980684b8b7df23460013afaace8d3a3a0ba
Nick Nguyen <nicholas.ngoc.nguyen@gmail.com>
2023-10-07 14:29:04 -0700

    Saved chosen are based on the list items

--- src/app.ts
+++ src/app.ts
@@ -70,21 +70,22 @@ async function init() {
   const theChosen = cards[randInt];
   console.log('I have chosen: ', theChosen);
 
-  Object.keys(chosenForFilenameFlag).forEach((listItem) => {
-    if (listItem === theChosen) {
-      chosenForFilenameFlag[listItem] = 1;
+  const updatedChosenForFilenameFlag = {};
+  list.forEach((listItem) => {
+    if (listItem === theChosen || !chosenForFilenameFlag[listItem]) {
+      updatedChosenForFilenameFlag[listItem] = 1;
     } else {
-      chosenForFilenameFlag[listItem] += 1
+      updatedChosenForFilenameFlag[listItem] = chosenForFilenameFlag[listItem] + 1
     }
   });
 
   console.log('New Top 3 Cards:')
-  const topThreeCards = list.sort((listItem) => chosenForFilenameFlag[listItem] ?? 0)
+  const topThreeCards = list.sort((listItem) => updatedChosenForFilenameFlag[listItem] ?? 0)
     .slice(0, 3)
-    .map(listItem => `${listItem}${chosenForFilenameFlag[listItem] ? `: ${chosenForFilenameFlag[listItem]}` : ''}`);
+    .map(listItem => `${listItem}${updatedChosenForFilenameFlag[listItem] ? `: ${updatedChosenForFilenameFlag[listItem]}` : ''}`);
   console.log(`[${topThreeCards.join(', ')}]`)
 
-  chosen[filenameFlag] = chosenForFilenameFlag;
+  chosen[filenameFlag] = updatedChosenForFilenameFlag;
   try {
     // TODO I can't get this to generate the file if it doesn't exist
     const fileHandle = await fsp.open(chosenFilepath, 'w');
