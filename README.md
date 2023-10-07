a9aadd0e4601e3e54cb1678b5783b636a2871683
Nick Nguyen <nicholas.ngoc.nguyen@gmail.com>
2023-10-07 04:27:13 -0700

    Make single chosen file and make passed in flags be based on list file name

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ Current supported flags are: ow, gsupps, halo
 
 Run with:
 1. npm run build
-2. npm start [FLAG]
+2. In the `lists` folder create text files for each list of each item to pick from. A template file has been provided
+3. npm start <LIST_FILE_NAME>
--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,13 @@
-import fs from 'fs';
 import fsp from 'fs/promises';
 import path from 'path';
 
-enum SupportedFlag {
-  OW = 'ow',
-  GSUPPS = 'gsupps',
-  HALO = 'halo'
-}
-// type SupportedFlag = 'ow' | 'gsupps' | 'halo';
-
-type FilepathByFlag = Record<SupportedFlag, string>;
-const chosenFilepathBySupportedFlags: FilepathByFlag = {
-  'ow': '../../chosen/chosenOw.json',
-  'gsupps': '../../chosen/chosenGsupps.json',
-  'halo': '../../chosen/chosenHalo.json'
+type ChosenFileFormat = {
+  [filenameFlag: string]: {
+    [item: string]: number
+  }
 }
 
-const listFilepathBySupportedFlags: FilepathByFlag = {
-  'ow': '../../lists/overwatch2.txt',
-  'gsupps': '../../lists/gsupps.txt',
-  'halo': '../../lists/halo.txt'
-}
-
-const chosenFilepath = '../../chosen'
-
-function getFlag(): string {
+function getFilenameFlag(): string {
   try {
     // Note the third arg will be the list flag
     return process.argv[2];
@@ -34,57 +17,33 @@ function getFlag(): string {
   }
 }
 
+const filenameFlag = getFilenameFlag();
+
 function getRelativePath(_path: string) {
   return path.resolve(process.argv[1], _path);
 }
 
+const chosenFilepath = getRelativePath('../../chosen.json');
+
 async function getList(): Promise<string[] | null> {
-  const flag = getFlag();
-  const listFilePath = listFilepathBySupportedFlags[flag];
+  const listFilePath = getRelativePath(`../../lists/${filenameFlag}.txt`);
   if (!listFilePath) {
     return null;
   }
 
-  const relativePath = getRelativePath(listFilePath);
-  const list = await fsp.readFile(relativePath, 'utf8');
+  const list = await fsp.readFile(listFilePath, 'utf8');
   return list.split('\r\n')
 }
 
-async function getChosen(list: string[]): Promise<{ [item: string]: number }> {
-  const chosen: { [item: string]: number } = {};
-  list.forEach((item) => {
-    chosen[item] = 1;
-  })
-
-  const flag = getFlag();
-  const chosenFilepath = chosenFilepathBySupportedFlags[flag];
-  if (chosenFilepath) {
-    const relativePath = getRelativePath(chosenFilepath);
-    if (fs.existsSync(relativePath)) {
-      try {
-        const content = await fsp.readFile(relativePath);
-        const chosenFromFile = JSON.parse(content.toString()) as any;
-        Object.keys(chosen).forEach((key) => {
-          if (chosenFromFile[key]) {
-            chosen[key] = chosenFromFile[key]
-          }
-        })
-      } catch (e) {
-        console.log('Error while reading file: ', e);
-      }
-    } else {
-      try {
-        // TODO I can't get this to work. It just errors instead of creating the file
-        const fileHandle = await fsp.open(relativePath, 'w');
-        await fileHandle.writeFile(JSON.stringify(chosen));
-        await fileHandle.close();
-      } catch (e) {
-        console.log('Error while creating chosen file', e);
-      }
-    }
+async function getChosen(list: string[]): Promise<ChosenFileFormat> {
+  try {
+    const fileHandle = await fsp.open(chosenFilepath, 'w+');
+    const content = await fileHandle.readFile();
+    return content.toString() ? JSON.parse(content.toString()) : {};
+  } catch (e) {
+    console.log('Error getting chosen', e);
+    throw e;
   }
-
-  return chosen;
 }
 
 async function init() {
@@ -94,30 +53,37 @@ async function init() {
   }
 
   const chosen = await getChosen(list);
+  const chosenForFilenameFlag = chosen[filenameFlag] ? chosen[filenameFlag] : {};
   const cards = [];
-  Object.keys(chosen).forEach((choice) => {
-    for (let i = 0; i < chosen[choice]; i += 1) {
-      cards.push(choice);
+  list.forEach((listItem) => {
+    if (!chosenForFilenameFlag[listItem]) {
+      chosenForFilenameFlag[listItem] = 1
+    }
+
+    for (let i = 0; i < chosenForFilenameFlag[listItem]; i += 1) {
+      cards.push(listItem);
     }
   })
+
   const randInt = Math.floor(Math.random() * cards.length);
   const theChosen = cards[randInt];
   console.log('I have chosen: ', theChosen);
 
-  Object.keys(chosen).forEach((choice) => {
-    if (choice === theChosen) {
-      chosen[choice] = 1;
+  console.log('New Cards:');
+  Object.keys(chosenForFilenameFlag).forEach((listItem) => {
+    if (listItem === theChosen) {
+      chosenForFilenameFlag[listItem] = 1;
     } else {
-      chosen[choice] += 1
+      chosenForFilenameFlag[listItem] += 1
     }
+
+    console.log(`${listItem}: ${chosenForFilenameFlag[listItem]}`);
   });
 
-  const flag = getFlag();
-  const chosenFilepath = chosenFilepathBySupportedFlags[flag];
-  const relativePath = getRelativePath(chosenFilepath);
+  chosen[filenameFlag] = chosenForFilenameFlag;
   try {
     // TODO I can't get this to generate the file if it doesn't exist
-    const fileHandle = await fsp.open(relativePath, 'w');
+    const fileHandle = await fsp.open(chosenFilepath, 'w');
     await fileHandle.writeFile(JSON.stringify(chosen));
     await fileHandle.close();
   } catch (e) {
